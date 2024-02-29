6d66c91c019cc2144d3d375efe3ff60809e18bfa
Edward Del Rosario <edwarddr23@gmail.com>
2024-02-29 15:35:04 -0500

    Academic years in Years screen can be successfully edited, but profile object is not modified.

--- screens/YearsScreen.tsx
+++ screens/YearsScreen.tsx
@@ -1,129 +1,101 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, TextInput } from 'react-native'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Feather from 'react-native-vector-icons/Feather'
 import Footer from '../shared/custom_footer'
 
 const YearView = ({year}) => {
     // useStates for TextInputs:
-    const[beg_year, setBeg_year] = useState("");
-    const[end_year, setEnd_year] = useState("");
-    // const[is_editing, setIs_editing] = useState(false);
-    let is_editing = false;
+    const[stateYear, setStateYear] = useState(year.item);
+    const[beg_year, setBeg_year] = useState(String(year.item.beg_year));
+    const[end_year, setEnd_year] = useState(String(year.item.end_year));
+    const[is_editing, setIs_editing] = useState(false);
 
-    console.log('YearView(): year.item:', year.item);
-
-    handleYearRender = () => {
+    const handleYearRender = () => {
         // console.log('handleYearRender(): is_editing:', is_editing);
-        if (is_editing === false) {
-            return <Feather name="edit" style={{color: "black", flex: 1}} size={24}/>
+
+        if (is_editing == false) {
+            return(
+                // <Feather name="edit" style={{color: "black", flex: 1}} size={24}/>
+                <View style={{paddingBottom: 10}}>
+                    <View style={styles.year}>
+                        {/* {renderLabel("Season")} */}
+                        <Text style={{textAlignVertical: 'center', fontSize: 20, flex: 1}}>Academic Year {stateYear.beg_year}-{stateYear.end_year}:</Text>
+                        <TouchableOpacity activeOpacity={0.5} 
+                            onPress={() => {
+                                setIs_editing(true);
+                            }}>
+                            <AntDesign name="edit" size={30} color="black"/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            );
         }
-        return <AntDesign name="checkcircleo" size={24} color="black"/>
+        return(
+            <View style={{paddingBottom: 10}}>
+              <View style={styles.year}>
+                  <Text style={{textAlignVertical: 'center'}}>Academic Year:</Text>
+                  {/*Beg Year Number */}
+                  <TextInput
+                      style={styles.inputText}
+                      value={beg_year}
+                      placeholder="Start"
+                      onChangeText={text => {
+                          setBeg_year(text);
+                      }}
+                      keyboardType="numeric"
+                  />
+                  {/*End Year Number */}
+                  <TextInput
+                      style={styles.inputText}
+                      value={end_year}
+                      placeholder="End"
+                      onChangeText={text => {
+                          setEnd_year(text);
+                      }}
+                      keyboardType="numeric"
+                  />
+                  <TouchableOpacity 
+                    onPress={() => {
+                        console.log('Done button: beg_year:', beg_year);
+                        console.log('Done button: end_year:', end_year);
+                        setStateYear({
+                            ...stateYear,
+                            beg_year: beg_year,
+                            end_year: end_year
+                        });
+                        setIs_editing(false);
+                    }}>
+                    <AntDesign name="checkcircleo" style={styles.plus_icon} size={30}/>
+                  </TouchableOpacity>
+              </View>
+            </View>
+        );
     }
 
-    return(
-        <View style={{paddingBottom: 10}}>
-            <View style={styles.year}>
-                {/* {renderLabel("Season")} */}
-                <Text style={{textAlignVertical: 'center', fontSize: 20, flex: 1}}>Academic Year {year.item.beg_year}-{year.item.end_year}:</Text>
-                <TouchableOpacity activeOpacity={0.5} onPress={() => {
-                  console.log('Add a huh?');
-                  is_editing = is_editing === false ? true : false
-                  // setIs_editing(is_editing === false ? true : false);
-                  console.log('AFTER is_editing:', is_editing);
-                  }}>
-                  { handleYearRender() }
-                </TouchableOpacity>
-                {/* <TouchableOpacity activeOpacity={0.5} onPress={() => {
-                    print('YearView: this ran');
-                    setIs_editing(!is_editing);
-                    print('YearView: is_editing:', is_editing);
-                  }}>
-                    <Feather name="edit" style={{color: "black", flex: 1}} size={24}/>
-                </TouchableOpacity> */}
-            </View>
-        </View>
-        // Editing Semester View:
-        // <View style={{paddingBottom: 10}}>
-        //   <View style={styles.year}>
-        //       {/* {renderLabel("Season")} */}
-        //       <Text style={{textAlignVertical: 'center'}}>Academic Year:</Text>
-        //       {/*Beg Year Number */}
-        //       <TextInput
-        //           style={styles.inputText}
-        //           value={beg_year}
-        //           placeholder="Start"
-        //           onChangeText={text => {
-        //               setBeg_year(text);
-        //           }}
-        //           onEndEditing={() => {
-        //               year.beg_year = beg_year;
-        //           }}
-        //           onSubmitEditing={() => {
-        //               year.beg_year = beg_year;
-        //           }}
-        //           keyboardType="numeric"
-        //       />
-        //       {/*End Year Number */}
-        //       <TextInput
-        //           style={styles.inputText}
-        //           value={end_year}
-        //           placeholder="End"
-        //           onChangeText={text => {
-        //               setEnd_year(text);
-        //           }}
-        //           onEndEditing={() => {
-        //               year.end_year = end_year;
-        //           }}
-        //           onSubmitEditing={() => {
-        //               year.end_year = end_year;
-        //           }}
-        //           keyboardType="numeric"
-        //       />
-        //       <TouchableOpacity onPress={() => {console.log('Add a huh?')}}>
-        //           <AntDesign name="pluscircleo" style={styles.plus_icon} size={30}/>
-        //       </TouchableOpacity>
-        //   </View>
-        // </View>
-    );
+    return(handleYearRender());
 }
 
 const YearsScreen = ({navigation, route}) => {
     const {profile} = route.params;
-    // navigation.setOptions({title: 'Academic Years: ' + profile["name"]});
-    // navigation.setParams({title: profile["name"]});
-    // const profile_name = profile.name;
-    // navigation.setParams({title: {profile_name}});
-    console.log('YearsScreen.tsx: profile:', profile);
-    console.log('YearsScreen.tsx: profile["name"]:', profile["name"]);
-    console.log('YearsScreen.tsx: profile["academic_years"]:', profile["academic_years"]);
-    for(academic_year of profile["academic_years"]){
-        console.log('YearsScreen.tsx: LOOP', {academic_year});
-        for(curr_class of academic_year["classes"]){
-            console.log('YearsScreen.tsx: LOOP curr_class:', {curr_class});
-        }
-    }
-
-    // return(
-    //     <View>
-    //         <Text>{route.params.profile["name"]}</Text>
-    //         {/* <Text>{route.params.profile_name}</Text> */}
-    //     </View>
-    // );
+    // console.log('YearsScreen.tsx: profile:', profile);
+    // console.log('YearsScreen.tsx: profile["name"]:', profile["name"]);
+    // console.log('YearsScreen.tsx: profile["academic_years"]:', profile["academic_years"]);
+    // for(academic_year of profile["academic_years"]){
+    //     console.log('YearsScreen.tsx: LOOP', {academic_year});
+    //     for(curr_class of academic_year["classes"]){
+    //         console.log('YearsScreen.tsx: LOOP curr_class:', {curr_class});
+    //     }
+    // }
     
     const [years, setYears] = useState(profile["academic_years"]);
-    console.log('INITIAL years:', years);
+    // console.log('INITIAL years:', years);
 
     const renderYear = ((curr_year) => {
-        console.log('renderYear(): curr_year:', curr_year);
+        // console.log('renderYear(): curr_year:', curr_year);
         return(
             // Render current semester in a custom SemesterView component
-            // <View style={{backgroundColor: 'purple'}}>
-                <YearView
-                year={curr_year}
-                //   key={curr_year.id}
-                />
-            // </View>
+            <YearView year={curr_year}/>
         );
     });
 
@@ -151,7 +123,7 @@ const YearsScreen = ({navigation, route}) => {
             <Text style={{fontSize: 17, fontWeight: 'bold', paddingBottom: 5}}>Press the plus sign to add an academic year</Text>
             <View style={{flex: 1, width: '100%', alignItems: 'center'}}>
                 <FlatList
-                    style={{width: '90%', flex: 1, backgroundColor: 'purple'}}
+                    style={{width: '90%', flex: 1}}
                     data={years}
                     renderItem={(curr_year => {
                         return renderYear(curr_year);
