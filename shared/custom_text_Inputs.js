6485fa1c9cf403567a8d48b613cbdeb24e0fcc07
Edward Del Rosario <edwarddr23@gmail.com>
2024-05-01 19:53:08 -0400

    finished documentation for custom_text_inputs.js

--- shared/custom_text_Inputs.js
+++ shared/custom_text_Inputs.js
@@ -1,25 +1,47 @@
+/* 
+    custom_text_inputs.js
+    PURPOSE
+
+        The purpose of this file is to define the custom text input component that will be used for almost all
+        the screens in this project. The idea is to use this modularly and add it to any relevant screens.
+*/
+
 import React, { useState } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 import common_style from './common_style';
 
+/*
+NAME
+
+    InputWithLabel - a function component that returns a View with a TextInput with some defaults already determined.
+
+SYNOPSIS
+
+    <View> InputWithLabel({style, textStyle, labelStyle, value, onChangeText, placeholder, hasLabel, label})
+        style --> an object containing all the additional styling that will be added to the View.
+        textStyle --> an object containing all the additional styling that will be added to the text within the child TextInput.
+        labelStyle --> an object containing all the additional styling that will be added to the label of the TextInput.
+        value --> a state object that will be placed into the TextInput's value property.
+        onChangeText --> a function that will be placed into the TextInput's onChangeText property.
+        placeholder --> a string whose value will be used for the TextInput's placeholder property.
+        hasLabel --> a boolean which will determine whether the label for the custom text input is displayed or not.
+        label --> a string whose value will be used for the label of the TextInput.
+
+DESCRIPTION
+
+    This is essentially a prewritten View component with predefined styling. On its own it is just a View component with a TextInput. The parameters
+    are injected into the View and its TextInput component, and the label is just a Text component.
+
+RETURNS
+
+    Returns a View component with a TextInput component, which has a predefined style.
+*/
 export const InputWithLabel = ({style, textStyle, labelStyle, value, onChangeText, placeholder, hasLabel, label}) => {
     const [isFocus, setIsFocus] = useState(false);
 
-    // const renderLabel = (label) => {
-    //     // console.log('custom_text_inputs.js: renderLabel(): value: ', value);
-    //     // console.log('custom_text_inputs.js: renderLabel(): isFocus: ', isFocus);
-    //     if (value || isFocus) {
-    //         // console.log('custom_text_inputs.js: renderLabel(): returning label...');
-    //         return(
-    //             <Text style={[styles.label, isFocus && {color: 'blue'}]}>{label + ":"}</Text>
-    //         );
-    //     }
-    //     return null;
-    // }
-    
     return(
         <View style={style}>
-            {/* {renderLabel(label)} */}
+            {/* Render the label for the custom text input. */}
             {hasLabel && (
                 <Text style={[styles.label, labelStyle]}>{label}</Text>
             )}
@@ -33,13 +55,6 @@ export const InputWithLabel = ({style, textStyle, labelStyle, value, onChangeTex
                 onChangeText={text => {
                     onChangeText(text)
                 }}
-                onSubmitEditing={() => {
-                    // console.log('custom :D');
-                    // console.log('SemesterView(): semester: ', semester);
-                    // console.log('BEFORE SemesterView(): semester.year: ', semester.year);
-                    // semester.year = year;
-                    // console.log('AFTER SemesterView(): semester.year: ', semester.year);
-                }}
             />
         </View>
     );
@@ -47,14 +62,9 @@ export const InputWithLabel = ({style, textStyle, labelStyle, value, onChangeTex
 
 const styles = StyleSheet.create({
     input: {
-        // height: 'auto',
-        // backgroundColor: 'red',
-        // margin: 12,
-        // marginHorizontal: 20,
         borderWidth: 1,
         borderRadius: 10,
         padding: 10,
-        // flex: 1,
         fontSize: 20,
         maxWidth: 'auto'
     },
