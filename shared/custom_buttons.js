32b02b87ab70daf87690d693b7808e077d379fc7
Edward Del Rosario <edwarddr23@gmail.com>
2024-05-01 19:41:09 -0400

    Finished documentation for custom_buttons.js.

--- shared/custom_buttons.js
+++ shared/custom_buttons.js
@@ -1,6 +1,35 @@
+/* 
+    custom_buttons.js
+    PURPOSE
+
+        The purpose of this file is to define the custom FlatButton component that will be used for UI elements
+        throughout the scope of this project. It has a common look and modular functionality that will be reused.
+*/
+
 import React from 'react'
 import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
 
+/*
+NAME
+
+    FlatButton - a function component that returns a component that will be a TouchableOpacity with some defaults already determined.
+
+SYNOPSIS
+
+    <TouchableOpacity> FlatButton({text, onPress})
+        text --> a string that represents the text to display on the button.
+        onPress --> a function that represents what the TouchableOpacity should do onPress.
+
+DESCRIPTION
+
+    This is essentially a prewritten TouchableOpacity component with predefined styling and with modular functionality. On its own,
+    it does not do much, but that is what the onPress parameter of this function is for. The parameter text is used as the displayed
+    text on the button, and the onPress parameter is used to define what the button does onPress.
+
+RETURNS
+
+    Returns a TouchableOpacity component with a predefined style.
+*/
 export default function FlatButton({ text, onPress }) {
     return(
         <TouchableOpacity onPress={ onPress } style={[styles.button]}>
@@ -12,15 +41,11 @@ export default function FlatButton({ text, onPress }) {
 const styles = StyleSheet.create({
     button: {
         backgroundColor: '#F01d71',
-        // height: 'auto',
-        // width: 'auto',
         alignSelf: 'flex-start',
-        // flex: 1,
         width: '100%',
         height: '100%',
         padding: 10,
         borderRadius: 10,
-        // borderWidth: 3,
         alignItems: 'center',
         justifyContent: 'center'
     },
@@ -30,7 +55,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         fontSize: 20,
-        // backgroundColor: 'blue',
         width: '100%',
         height: '100%',
         color: 'white'
