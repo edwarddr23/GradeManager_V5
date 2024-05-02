cd342122e7d39c443462b3053338797f81ed0a62
Edward Del Rosario <edwarddr23@gmail.com>
2024-05-01 20:03:38 -0400

    finished documentation for key_functions.tsx

--- shared/key_functions.tsx
+++ shared/key_functions.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
+/* 
+    key_functions.tsx
+    PURPOSE
 
-// Initializes an array's elements so that each one has a unique id.
-// export const initializeArrKeys = (arr) => {
-//     if(arr.length > 0) return arr;
-//     // let initArr = [...arr];
-//     let tmp_nextId = 0;
-//     return(arr.map(element => {
-//         let tmpElement ={
-//             ...element,
-//             id: tmp_nextId
-//         };
-//         tmp_nextId++;
-//         return tmpElement;
-//     }));
-// }
+        The purpose of this file is to define logic for finding the next id from an array of objects. This
+        will be sued whenever a new object is created which needs to be recorded in the backend. Thus, this
+        is shared globally as it will be used many times within the scope of this project.
+*/
 
-// Finds the maximum ID in an array and returns that max ID + 1 to ensure that the ID returned is the next unique ID.
+/*
+NAME
+
+    findNextID - a function component that finds the maximum ID in an array and returns that max ID + 1 to ensure that the ID returned is the next unique ID.
+
+SYNOPSIS
+
+    int findNextID(arr)
+        arr --> an array, of presumably objects, to determine the next ID for.
+
+DESCRIPTION
+
+    Iterate through every element within parameter arr. If the id in the current element in question is greater than the maxID,
+    then that is the new maxID. After all elements have been considered, the maxID + 1 will be returned, to ensure that the next
+    ID is different.
+
+RETURNS
+
+    Returns an int that represents the next id for a new object in the parameter arr.
+*/
 export const findNextID = (arr) => {
     if(arr.length == 0) return 0;
     let maxID = 0;
