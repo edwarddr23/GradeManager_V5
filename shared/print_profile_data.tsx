20e2b6f7af631b0f27efd039fbedc246b13e81a0
Edward Del Rosario <edwarddr23@gmail.com>
2024-05-08 18:56:42 -0400

    Finished documentation for print_profile_data.tsx

--- shared/print_profile_data.tsx
+++ shared/print_profile_data.tsx
@@ -21,18 +21,17 @@ NAME
     
 SYNOPSIS
 
-    <View> findNextID(arr)
-        arr --> an array, of presumably objects, to determine the next ID for.
+    <View> PrintSemesters(year)
+        year --> a YearContent object whose semesters' info will be outputted to the <View>.
 
 DESCRIPTION
 
-    Iterate through every element within parameter arr. If the id in the current element in question is greater than the maxID,
-    then that is the new maxID. After all elements have been considered, the maxID + 1 will be returned, to ensure that the next
-    ID is different.
+    Iterate through parameter year's semesters and output their info depending on whether it is properly initialized or not
+    (whether the semester's season has been defined). Each semester's season, year, and classes are displayed.
 
 RETURNS
 
-    Returns an int that represents the next id for a new object in the parameter arr.
+    Returns a View component that shows information from the parameter year's semesters.
 */
 function PrintSemesters(year) {
     let child_key = 0;
@@ -57,6 +56,25 @@ function PrintSemesters(year) {
     );
 }
 
+/*
+NAME
+
+    PrintClasses - a function that prints the classes found in parameter semester.
+    
+SYNOPSIS
+
+    <View> PrintClasses(semester)
+        semester --> a SemesterContent object whose class' info will be outputted to the <View>.
+
+DESCRIPTION
+
+    Iterate through parameter semester's classes and output their info depending on whether it is properly initialized or not
+    (whether the class's name has been defined). Each class's name, letter grade, and sections are displayed.
+
+RETURNS
+
+    Returns a View component that shows information from the parameter semester's classes.
+*/
 function PrintClasses(semester) {
     let child_key = 0;
 
@@ -80,6 +98,25 @@ function PrintClasses(semester) {
     )
 }
 
+/*
+NAME
+
+    PrintSections - a function that prints the sections found in parameter curr_class.
+    
+SYNOPSIS
+
+    <View> PrintSections(curr_class)
+        curr_class --> a ClassContent object whose sections' info will be outputted to the <View>.
+
+DESCRIPTION
+
+    Iterate through parameter curr_class's sections and output their info depending on whether there are sections
+    in the class or not. Each section's name, relative weight, and assignments are displayed.
+
+RETURNS
+
+    Returns a View component that shows information from the parameter curr_class's sections.
+*/
 function PrintSections(curr_class) {
     let child_key = 0;
     if(curr_class.sections === undefined || curr_class.sections.length == 0){
@@ -114,6 +151,25 @@ function PrintSections(curr_class) {
     );
 }
 
+/*
+NAME
+
+    PrintAssignments - a function that prints the assignments found in parameter section.
+    
+SYNOPSIS
+
+    <View> PrintAssignments(section)
+        section --> a SectionContent object whose assignments' info will be outputted to the <View>.
+
+DESCRIPTION
+
+    Iterate through parameter section's assignments and output their info depending on whether there are assignments
+    in the section or not. Each assignment's name, grade, or numerator and denominator are displayed.
+
+RETURNS
+
+    Returns a View component that shows information from the parameter section's assignments.
+*/
 function PrintAssignments(section) {
     let child_key = 0;
     if(section.assignments.length === 0){
@@ -124,7 +180,6 @@ function PrintAssignments(section) {
     else{
         return(
             <View>
-                {/* <Text style={common_style.defaultText}>{'\t\t\t\t'}Assignments:</Text> */}
                 {section.assignments.map((a) => {
                     child_key++;
                     if(a.name === ''){
@@ -153,9 +208,27 @@ function PrintAssignments(section) {
     }
 }
 
+/*
+NAME
+
+    PrintData - a function component that prints the academic years and their childrens' data found in parameter profile_context.
+    
+SYNOPSIS
+
+    <View> PrintData(profile_context)
+        profile_context --> a ProfileContent object whose academic years' info will be outputted to the <View>.
+
+DESCRIPTION
+
+    Iterate through parameter profile_context's academic years and output their info. Each academic year's beginning year, end 
+    year, and semesters are displayed.
+
+RETURNS
+
+    Returns a View component that shows information from the parameter profile_context's academic years.
+*/
 // The Profile context is passed in rather than called within this component as it led to inconsistent hook calls from the caller.
 export const PrintData = (profile_context) => {
-    // console.log('PrintData(): profile_context.years:', profile_context.years);
     let child_key = 0;
 
     return(
@@ -163,7 +236,6 @@ export const PrintData = (profile_context) => {
             <Text style={[common_style.defaultText, {fontWeight: 'bold', textAlign: 'center'}]}>Profile Data for "{profile_context.profile_name}":</Text>
             {profile_context.years.map((year) => {
                 child_key++
-                // console.log(`PrintData(): year: ${year}`);
                 return(
                     <View key={child_key}>
                         {year.beg_year === -1 && (
@@ -173,12 +245,9 @@ export const PrintData = (profile_context) => {
                             <Text style={[common_style.defaultText, {fontWeight: 'bold'}]}>Academic Year {year.beg_year}-{year.end_year}:</Text>
                         )}
                         { PrintSemesters(year) }
-                        {/* {PrintClasses(year)} */}
                     </View>
                 );
             })}
-            {/* {PrintAcademicYrs(profile)} */}
         </View>
-        // <Text style={common_style.defaultText}>Hello</Text>
     );
 }
